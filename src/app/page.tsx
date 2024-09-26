c5a8c0cd833d88749e3d25a3b614f1d56bc3da02
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2024-09-26 17:05:27 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import GitHubProfileViewer from "@/components/ui/github-profile";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <GitHubProfileViewer/>
+    </div>
+  );
+}
